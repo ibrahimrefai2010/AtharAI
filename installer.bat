d9c9a65544937a9b1d2fc5b00ac28619fd2a3025
Ibrahim Refai <109442477+ibrahimrefai2010@users.noreply.github.com>
2024-04-10 02:43:49 +0300

    Delete installer.bat

--- installer.bat
+++
@@ -1,9 +0,0 @@
-winget uninstall python
-winget install Python.Python.3.10
-
-winget install ffmpeg
-
-
-C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python310\python.exe -m pip install psutil SpeechRecognition pyaudio openai==0.28 pyttsx3 customtkinter flask waitress
-C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python310\python.exe -m pip install -q -U google-generativeai
-C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python310\python.exe -m pip install -U openai-whisper
